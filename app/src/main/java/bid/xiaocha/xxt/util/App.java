6adb6b38d5138ab2ad76988bda583e89831c85f7
xiaochacha <550399874@qq.com>
2017-10-12 20:00:45 +0800

    Initial commit

---
+++ app/src/main/java/bid/xiaocha/xxt/util/App.java
@@ -0,0 +1,14 @@
+package bid.xiaocha.xxt.util;
+
+import android.app.Application;
+
+/**
+ * Created by ssx on 2017/10/11.
+ */
+
+public class App extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
